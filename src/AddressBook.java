868b6e62ca5b5b031172f5bf7181417319d2bb41
random <randomhygaard@cmail.carleton.ca>
2025-09-18 08:12:00 -0400

    added output

---
+++ src/AddressBook.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList buddyList;
+
+    public AddressBook(){
+
+    }
+
+    public static void main(){
+        System.out.println("Address Book");
+    }
+    public void addBuddy(){}
+
+    public void removeBuddy(){}
+}
