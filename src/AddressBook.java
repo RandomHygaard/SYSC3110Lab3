5eba9d451a7de0d5cf650ad88a0c8aa993b09f5b
random <randomhygaard@cmail.carleton.ca>
2025-09-18 10:10:46 -0400

    feat: add hello print statement

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -12,6 +12,7 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        System.out.println("hello");
     }
     public void addBuddy(BuddyInfo buddy){myBuddies.add(buddy);}
 
