16ae7c1bf7f8ebe7f022895032194c4918ff9bae
random <randomhygaard@cmail.carleton.ca>
2025-09-25 08:33:09 -0400

    feat: changed final print from hello to this code changed.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -7,14 +7,16 @@ public class AddressBook {
         this.myBuddies = new ArrayList<>();
     }
 
+    public void addBuddy(BuddyInfo buddy){myBuddies.add(buddy);}
+
+    public void removeBuddy(BuddyInfo buddy){myBuddies.remove(buddy);}
+
     public static void main(String[] args){
         BuddyInfo buddy = new BuddyInfo("tod", "carton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
-        System.out.println("hello");
+        System.out.println("this code changed");
     }
-    public void addBuddy(BuddyInfo buddy){myBuddies.add(buddy);}
 
-    public void removeBuddy(BuddyInfo buddy){myBuddies.remove(buddy);}
 }
